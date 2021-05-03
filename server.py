fc0a2fd20fe251513da7a09cf65b7b2a697dfc3f
Gishan Chaminga Abeysinghe <gishan.abeysinghe@gmail.com>
2021-05-03 15:03:03 +0530

    fixed the logic and added api_key to the url

--- server.py
+++ server.py
@@ -15,12 +15,13 @@ def hello_world():
     return 'Hello, World!'
 
 @app.route('/currency', methods=['POST'])
-@cache.cached(timeout=86400)
 def currnecy_convert():
     data = json.loads(request.data)
     fromCurrency = data["fromCurrency"]
     amount = data["amount"]
     toCurrency = data["toCurrency"]
+    error = False
+    result = None
 
     fromValueEUR = cache.get(fromCurrency) 
     toValueEUR =  cache.get(toCurrency) 
@@ -28,23 +29,38 @@ def currnecy_convert():
     API_KEY = config["API_KEY"]
     
     if fromValueEUR is None and toValueEUR is None:
-        r = requests.get("http://data.fixer.io/api/latest?access_key=b08103793b70e9d70684b581c1fd31ea&symbols=" + fromCurrency + "," + toCurrency)
-        fromValueEUR = r.json()["rates"][fromCurrency]
-        toValueEUR = r.json()["rates"][toCurrency]
+        r = requests.get("http://data.fixer.io/api/latest?access_key=" + API_KEY + "&symbols=" + fromCurrency + "," + toCurrency)
+        if r.json()["success"]:
+            fromValueEUR = r.json()["rates"][fromCurrency]
+            toValueEUR = r.json()["rates"][toCurrency]
+            cache.set(fromCurrency, fromValueEUR, timeout=86400)
+            cache.set(toCurrency, toValueEUR, timeout=86400)
+        else: 
+            error = True
     elif fromValueEUR is None: 
-        r = requests.get("http://data.fixer.io/api/latest?access_key=b08103793b70e9d70684b581c1fd31ea&symbols=" + fromCurrency)
-        fromValueEUR = r.json()["rates"][fromCurrency]
+        r = requests.get("http://data.fixer.io/api/latest?access_key=" + API_KEY + "&symbols=" + fromCurrency)
+        if r.json()["success"]:
+            fromValueEUR = r.json()["rates"][fromCurrency]
+            cache.set(fromCurrency, fromValueEUR, timeout=86400)
+        else:
+            error = True
     elif toValueEUR is None:
-        r = requests.get("http://data.fixer.io/api/latest?access_key=b08103793b70e9d70684b581c1fd31ea&symbols=" + toCurrency)
-        fromValueEUR = r.json()["rates"][toValueEUR]
+        r = requests.get("http://data.fixer.io/api/latest?access_key=" + API_KEY + "&symbols=" + toCurrency)
+        if r.json()["success"]:
+            fromValueEUR = r.json()["rates"][toValueEUR]
+            cache.set(toCurrency, toValueEUR, timeout=86400)
+        else:
+            error = True
     else:
         print('all found in cache')
 
-    convertedAmount = (amount / fromValueEUR) * toValueEUR
+    if not error:
+        convertedAmount = (amount / fromValueEUR) * toValueEUR
 
-    result = {
-        "amount": convertedAmount,
-	    "currency": toCurrency
-    }
-
+        result = {
+            "amount": convertedAmount,
+            "currency": toCurrency
+        }, 200
+    else:
+        result = { error: 'please check the fixer api' }, 400
     return result
