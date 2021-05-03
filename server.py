d3ae33d2d13bfe6bf85f10ad8e507df48a8ff8fc
Gishan Chaminga Abeysinghe <gishan.abeysinghe@gmail.com>
2021-05-03 14:09:15 +0530

    api_key from .env

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, json
 import requests
 from flask_caching import Cache
+from dotenv import dotenv_values
 
 app = Flask(__name__)
 
 cache = Cache()
 app.config['CACHE_TYPE'] = 'simple'
 cache.init_app(app)
+config = dotenv_values(".env")
 
 @app.route('/')
 def hello_world():
@@ -23,7 +25,7 @@ def currnecy_convert():
     fromValueEUR = cache.get(fromCurrency) 
     toValueEUR =  cache.get(toCurrency) 
 
-    API_KEY = "2e70aaac34352ee4b4d69286fb7064c0"
+    API_KEY = config["API_KEY"]
     
     if fromValueEUR is None and toValueEUR is None:
         r = requests.get("http://data.fixer.io/api/latest?access_key=b08103793b70e9d70684b581c1fd31ea&symbols=" + fromCurrency + "," + toCurrency)
